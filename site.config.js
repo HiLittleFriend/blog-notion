00c55cf48af3440562c4c48de58d03965e6cab34
LittleFriend <18161869173@163.com>
2021-03-17 17:07:09 +0800

    update name

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'HiLittleFriend',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
